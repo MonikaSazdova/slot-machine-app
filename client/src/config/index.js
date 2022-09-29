21c8a43892c6ca4d078983e74ad2067ba9fb9adb
Monika <monika.sazdova@sourcico.com>
2022-09-29 13:07:32 +0200

    Clean up unused files

--- client/src/config/index.js
+++
@@ -1,9 +0,0 @@
-
-//Slot machine reels values
-export const SLOT_MACHINE = {
-  reel1: ["cherry", "lemon", "apple", "banana"],
-  reel2: ["lemon", "apple", "cherry", "banana"],
-  reel3: ["apple", "cherry", "banana", "lemon"],
-};
-//time for every spin of the reels
-export const REELS_SPINNING_TIMER = 10;
